789e2eaef24661a658c3075bc7fb1813d7e8a439
tyawaa <tyaraxd.06@gmail.com>
2025-11-25 20:42:22 +0700

    edit : Position.java

---
+++ model/map/Position.java
@@ -0,0 +1,47 @@
+public class Position {
+    private int x;  // kolom
+    private int y;  // baris
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+    
+    public Position add(int dx, int dy) {
+        return new Position(this.x + dx, this.y + dy);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Position)) return false;
+        Position other = (Position) obj;
+        return this.x == other.x && this.y == other.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return (31 * x) + y;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + "," + y + ")";
+    }
+}
