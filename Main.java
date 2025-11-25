44b834fc49c456e27fab11ae9ffb25a52e4ece5f
tyawaa <tyaraxd.06@gmail.com>
2025-11-25 11:56:04 +0700

    git commit -m "Membuat jendela game (window)"

---
+++ Main.java
@@ -0,0 +1,22 @@
+
+
+import javax.swing.JFrame; 
+
+public class Main{
+    public static void main(String[] args) {
+        JFrame window = new JFrame(); 
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("GOSONG");
+
+        GamePanel gamePanel = new GamePanel(); 
+        window.add(gamePanel); 
+
+        window.pack(); //Causes this window to be sized to fit the prefered size 
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        gamePanel.startGameThread();
+    }
+}
