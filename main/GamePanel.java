540d5a34d8a4e8801ddf19f88ee8039acfb1e7e2
ibr4him-ctrl <ibrahimsteik24@gmail.com>
2025-11-25 12:19:17 +0700

    Menambahkan kelas GamePanel dengan metode startGameThread yang belum diimplementasikan

---
+++ main/GamePanel.java
@@ -0,0 +1,10 @@
+package main;
+
+public class GamePanel {
+
+    public void startGameThread() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'startGameThread'");
+    }
+
+}
