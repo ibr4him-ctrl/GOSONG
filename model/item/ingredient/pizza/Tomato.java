e385788b6f551b2c1685937342aad0fbefc724b0
ibr4him-ctrl <ibrahimsteik24@gmail.com>
2025-11-25 20:34:37 +0700

    mengisi sausage dan tomato

--- model/item/ingredient/pizza/Tomato.java
+++ model/item/ingredient/pizza/Tomato.java
@@ -1,5 +1,61 @@
 package model.item.ingredient.pizza;
 
-public class Tomato {
-    
+import model.item.Item;
+import model.enums.IngredientState;
+
+public class Tomato extends Item {
+    private IngredientState state;
+    private boolean isCooked;
+    private boolean isBurned;
+
+    public Tomato() {
+        super("Tomato");
+        this.state = IngredientState.UNCHOPPED; // Awalnya tomat belum dipotong
+        this.isCooked = false;
+        this.isBurned = false;
+    }
+
+    public void chop() {
+        if (state == IngredientState.UNCHOPPED) {
+            state = IngredientState.CHOPPED;
+        }
+    }
+
+    public void cook() {
+        if (!isBurned) {
+            if (isCooked) {
+                isBurned = true;
+                isCooked = false;
+            } else {
+                isCooked = true;
+            }
+        }
+    }
+
+    public IngredientState getState() {
+        return state;
+    }
+
+    public boolean isChopped() {
+        return state == IngredientState.CHOPPED;
+    }
+
+    public boolean isCooked() {
+        return isCooked && !isBurned;
+    }
+
+    public boolean isBurned() {
+        return isBurned;
+    }
+
+    @Override
+    public String toString() {
+        String status = "";
+        if (isBurned) {
+            status = "burned ";
+        } else if (isCooked) {
+            status = "cooked ";
+        }
+        return status + (state == IngredientState.CHOPPED ? "chopped " : "") + super.toString();
+    }
 }
