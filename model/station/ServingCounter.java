b493cf92496223076ce3149f908bddbb4209901a
Leticia Aldina <18223108@std.stei.itb.ac.id>
2025-11-29 17:09:54 +0700

    update: revisi serving import plate

--- model/station/ServingCounter.java
+++ model/station/ServingCounter.java
@@ -1,8 +1,35 @@
 package model.station;
 
-public class ServingCounter extends Station{
-    private OrderManager orderManager;
-    private ScoreManager scoreManager;
-    
-    public void interact(Chef chef);
+import model.chef.Chef;
+import model.item.Item;
+import model.item.utensils.Plate;
+
+public class ServingCounter extends Station {
+
+    public ServingCounter(int x, int y) {
+        super(x, y, "Serving");
+    }
+
+    @Override
+    public String getSymbol() {
+        return "S";
+    }
+
+    @Override
+    public boolean interact(Chef chef) {
+        Item hand = chef.getHeldItem();
+
+        if (!(hand instanceof Plate plate)) {
+            return false;
+        }
+
+        if (plate.isEmpty()) {
+            return false;
+        }
+
+        plate.markDirty();
+        chef.setHeldItem(null);
+
+        return true;
+    }
 }
