b1de3cb4633b7beadaf757c4ef5012ed63397eb8
tyawaa <tyaraxd.06@gmail.com>
2025-11-29 13:44:13 +0700

    feat : make chef (tapi utk helditem saja utk bagian station)

--- model/chef/Chef.java
+++ model/chef/Chef.java
@@ -1,5 +1,22 @@
 package model.chef;
 
+import model.item.Item; 
 public class Chef {
-    
+    private Item heldItem; 
+
+    public Chef(){
+        this.heldItem = null; 
+    }
+
+    public Chef(Item initialItem){
+        this.heldItem = initialItem; 
+    }
+
+    public Item getHeldItem(){
+        return heldItem; 
+    }
+
+    public void setHeldItem(Item item){
+        this.heldItem = item; 
+    }
 }
