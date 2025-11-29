d75cf0da2f95b25119d6f067c3bdd4d40773a279
Leticia Aldina <18223108@std.stei.itb.ac.id>
2025-11-29 12:29:34 +0700

    feat: menambahkan abstract class Station

--- model/station/Station.java
+++ model/station/Station.java
@@ -1,21 +1,93 @@
 package model.station;
 
+import model.item.Item;
+import model.chef.Chef;
+
 public abstract class Station {
-    protected ;//Positionposition
-    protected ;//Item placedItem;
-
-    public abstract void interact (Chef chef);
-    //public abstract void update(float deltaTime);
-
-    public Position getPosition(){
-        return position;
+    protected int posX;
+    protected int posY;
+    protected String stationType;
+    protected Item itemOnStation;
+    
+    public Station(String type) {
+        //this.posX = x;
+        //this.posY = y;
+        this.stationType = type;
+        this.itemOnStation = null;
     }
 
-    public Item getPlaced(){
-        return placedItem;
+    //ini buat ngecek kalo chef udah deket sama station ato blom
+    // public boolean isAdjacentTo(Chef chef) {
+       //int chefX = chef.getPosition().getX();
+        //int chefY = chef.getPosition().getY();
+        //int deltaX = Math.abs(chefX - posX); //cek arah
+        //int deltaY = Math.abs(chefY - posY);
+        //return (deltaX == 1 && deltaY == 0) || (deltaX == 0 && deltaY == 1);}
+    
+    public abstract boolean interact(Chef chef);
+    public abstract String getSymbol();
+
+    public boolean placeItem(Item item) {
+        if (itemOnStation != null) {
+            return false; 
+        }
+        itemOnStation = item;
+        return true;
     }
 
-    public void setPlacedItem(Item item){
-        this.placedItem = item;
+    public Item takeItem() {
+        Item temp = itemOnStation;
+        itemOnStation = null;
+        return temp;
+    }
+    
+    public boolean isEmpty() {
+        return itemOnStation == null;
+    }
+    
+    public String getStatusDisplay() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("=== ").append(stationType.toUpperCase()).append(" ===\n");
+        sb.append("Position: (").append(posX).append(", ").append(posY).append(")\n");
+        sb.append("Status: ").append(isEmpty() ? "KOSONG" : "TERISI").append("\n");
+        
+        if (!isEmpty()) {
+            sb.append("Item: ").append(itemOnStation.getName()).append("\n");
+        }
+        
+        return sb.toString();
+    }
+    
+    //public boolean canBeUsed(Chef chef) {
+    //   return isAdjacentTo(chef);
+    //}
+    
+    //public int getPosX() {
+        //return posX;}
+    
+    //public int getPosY() {
+      //  return posY;}
+    
+    public String getStationType() {
+        return stationType;
+    }
+    
+    public Item getItemOnStation() {
+        return itemOnStation;
+    }
+    
+    public void setItemOnStation(Item item) {
+        this.itemOnStation = item;
+    }
+
+    public boolean isWalkable() {
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s di (%d, %d) - %s", 
+            stationType, posX, posY, 
+            isEmpty() ? "Kosong" : "punya " + itemOnStation.getName());
     }
 }
