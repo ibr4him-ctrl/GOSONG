b6fc82c8acff5ad868ba76c07edc5aac9669e350
larashtm <19623239@std.stei.itb.ac.id>
2025-11-29 18:20:06 +0700

    feat: menambah file handler dan constants

--- util/Constants.java
+++ util/Constants.java
@@ -1,5 +1,27 @@
 package util;
 
-public class Constants {
-    
+public final class Constants {
+
+    public static final int TILE_SIZE = 32;  
+    public static final float COLLISION_EPSILON = 0.1f;
+    public static final float PLAYER_SPEED = 2.5f;  
+
+    public static final float UP_X = 0f;
+    public static final float UP_Y = -1f;
+
+    public static final float DOWN_X = 0f;
+    public static final float DOWN_Y = 1f;
+
+    public static final float LEFT_X = -1f;
+    public static final float LEFT_Y = 0f;
+
+    public static final float RIGHT_X = 1f;
+    public static final float RIGHT_Y = 0f;
+
+    // Batas game world 
+    public static final int WORLD_WIDTH = 25;   
+    public static final int WORLD_HEIGHT = 20;  
+
+    // Prevent instantiation
+    private Constants() {}
 }
--- util/FileHandler.java
+++ util/FileHandler.java
@@ -1,5 +1,36 @@
 package util;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class FileHandler {
-    
+    public static String readFile(String path) throws IOException {
+        StringBuilder builder = new StringBuilder();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                builder.append(line).append("\n");
+            }
+        }
+
+        return builder.toString();
+    }
+
+    public static void writeFile(String path, String content) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(path))) {
+            writer.write(content);
+        }
+    }
+
+    public static void appendToFile(String path, String content) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(path, true))) {
+            writer.write(content);
+            writer.newLine();
+        }
+    }
 }
