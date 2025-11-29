ce8d03d007740e40a4fbf0723a8b04f281c8e366
Leticia Aldina <18223108@std.stei.itb.ac.id>
2025-11-29 18:51:53 +0700

    update: menambahkan trash station

--- model/station/TrashStation.java
+++ model/station/TrashStation.java
@@ -1,5 +1,40 @@
+// model/station/TrashStation.java
 package model.station;
 
-public class TrashStation extends Station{
-    public void interact(Chef chef);
+import model.chef.Chef;
+import model.enums.ItemType;
+import model.interfaces.CookingDevice;
+import model.item.Item;
+import model.item.utensils.Oven;
+
+public class TrashStation extends Station {
+
+    public TrashStation(int x, int y) {
+        super(x, y, "Trash");
+    }
+
+    @Override
+    public String getSymbol() {
+        return "T";
+    }
+
+    @Override
+    public boolean interact(Chef chef) {
+        Item hand = chef.getHeldItem();
+        if (hand == null) return false;
+
+        if (hand.getItemType() == ItemType.KITCHEN_UTENSIL) {
+            if (hand instanceof Oven oven) {
+                oven.clearContents(); // kamu sudah punya clearContents() di Oven
+                // utensil tetap di tangan Chef
+                return true;
+            }
+            // untuk utensil lain, nanti bisa dibuat method clear sendiri
+            return false;
+        }
+
+        // Selain itu (ingredient / dish / dsb.) → buang item-nya
+        chef.setHeldItem(null);
+        return true;
+    }
 }
